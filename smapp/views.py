73027dd6b686e76ae41900dcd0953f7e8f6a44be
claudio <info@smeat.com>
2022-03-11 22:44:17 +0100

    ok

--- smapp/views.py
+++ smapp/views.py
@@ -4,18 +4,11 @@ from .models import ip
 
 
 # Create your views here.
-def client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ipaddress = x_forwarded_for.split(',')[-1].strip()
-    else:
-        ipaddress = request.META.get('REMOTE_ADDR')
-    return ipaddress
+
 
 def home_page(request):
     first_ip=request.META.get('REMOTE_ADDR')
-    second_ip=client_ip(request)
-    return render(request, 'smapp/home_page.html', {'ip1':first_ip,'ip2':second_ip})
+    return render(request, 'smapp/home_page.html', {'ip1':first_ip})
 
 def lot_details(request):
     if request.method == "POST":
