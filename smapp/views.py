0fbdb7b42053c4ab201a6044e16a9ee6aa2b0fb1
claudio <info@smeat.com>
2022-03-14 10:55:59 +0100

    ip

--- smapp/views.py
+++ smapp/views.py
@@ -19,7 +19,7 @@ def home_page(request):
     current_ip=client_ip(request)
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        old_ip = x_forwarded_for.split(',')[-1].strip()
+        old_ip = x_forwarded_for.split(',')[-2].strip()
     else:
         old_ip = request.META.get('REMOTE_ADDR')
     return render(request, 'smapp/home_page.html', {'ip1':current_ip,'ip2':old_ip})
